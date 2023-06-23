438715622aaa131344abef672494d33371ca7109
Brian Manoti <brian.manoti@student.moringaschool.com>
2023-06-23 10:03:41 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function (){
+    const myElement = document.getElementById('text');
+
+    myElement.textContent = 'This is really cool!';
+
+
+})
